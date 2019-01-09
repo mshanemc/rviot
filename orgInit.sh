a65d832e9ae6d652b374004bb8f1a67862c4c06c
mshanemc <shane.mclaughlin@salesforce.com>
2019-01-09 10:00:10 -0600

    no -u allowed on shane:org:create

--- orgInit.sh
+++ orgInit.sh
@@ -1,11 +1,11 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 10 -s --json -u iot -o tdx.booth
+sfdx shane:org:create -f config/project-scratch-def.json -d 10 -s --json --userprefix iot -o tdx.booth
 sfdx force:source:push
 sfdx force:user:permset:assign -n IoT_Perms
 sfdx force:data:tree:import -p data/ImportPlan.json
 sfdx shane:user:password:set -l User -g User -p sfdx1234
 sfdx force:org:open
 
-sfdx force:user:create -a iotUser firstname=Lippert lastname=IoT permsets=IoT_Perms profileName="System Administrator"
+sfdx force:user:create --definitionfile config/userDef/lippert-iot-user-def.json
 sfdx shane:user:password:set -l IoT -g Lippert -p sfdx1234
 
 sfdx shane:iot:activation -n RV_Component_Monitoring -r
